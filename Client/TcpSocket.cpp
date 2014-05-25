c387377f6039b6998e85552276ec9c4eee9884e0
Christoph <cheichler@uos.de>
2014-05-25 23:46:52 +0200

    Corrected TcpSocket.cpp

--- Client/TcpSocket.cpp
+++ Client/TcpSocket.cpp
@@ -33,8 +33,8 @@ bool TcpSocket::Accept(TcpSocket &clientSock)
 		(struct sockaddr *) &clientSock.m_SockAddr, (socklen_t *) &size);
 	if (clientSock.m_Sock == -1)
 		cout << "accept failed: " << WSAGetLastError() << endl;
-	printf("Accepting\n\n\n\n\n\n\n\n\n");
-	//WSACleanup();
+	WSACleanup();
+	return false;
 	return true;
 }
 
